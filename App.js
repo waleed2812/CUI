e37e703a800059581ab755eee1b3a87d767269ca
waleedbutt98 <waleed3072@gmail.com>
2020-12-25 10:00:07 +0500

    Navigation Added

--- App.js
+++ App.js
@@ -1,114 +1,172 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+/* eslint-disable react-native/no-inline-styles */
+import * as React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {Button} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import {Ionicons} from '@expo/vector-icons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+// Constants
+import {styles} from './constants/style';
+import {View, Text} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// My Components
 
-const App: () => React$Node = () => {
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomTabs = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+    <Tab.Navigator
+      initialRouteName={'Contacts'}
+      activeColor="#f0edf6"
+      inactiveColor="#3e2465"
+      shifting={true}
+      barStyle={{backgroundColor: '#694fad', paddingBottom: 10}}>
+      <Tab.Screen
+        name="Contacts"
+        component={() => {
+          return (
+            <View style={styles.container}>
+              <Text>Bottom Tab 1</Text>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+          );
+        }}
+        options={{
+          tabBarColor: 'green',
+          tabBarIcon: ({color}) => (
+            <Ionicons name={'people'} size={20} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="History"
+        component={() => {
+          return (
+            <View style={styles.container}>
+              <Text>Bottom Tab 2</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+          );
+        }}
+        options={{
+          tabBarBadge: true,
+          tabBarColor: 'pink',
+          tabBarIcon: ({color}) => (
+            <FontAwesome name={'history'} size={20} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const Stack = createStackNavigator();
+
+const MyStackNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={({navigation}) => ({
+        headerTitleAlign: 'center',
+        headerTintColor: 'white',
+        headerStyle: {
+          backgroundColor: 'lightblue',
+        },
+        headerLeft: () => (
+          <Ionicons
+            name={'menu'}
+            color={'white'}
+            size={40}
+            onPress={navigation.toggleDrawer}
+          />
+        ),
+      })}>
+      <Stack.Screen
+        name="Home"
+        component={() => {
+          return (
+            <View style={styles.container}>
+              <Text>Bottom Tab 1</Text>
+            </View>
+          );
+        }}
+        options={{
+          title: 'Welcome',
+          // headerShown: false,
+          headerTitleAlign: 'center',
+          headerTintColor: 'white',
+          headerStyle: {
+            backgroundColor: 'blue',
+          },
+          headerRight: () => <Button title={'Edit'} />,
+        }}
+      />
+      <Stack.Screen
+        name="Start"
+        component={() => {
+          return (
+            <View style={styles.container}>
+              <Text>Bottom Tab 1</Text>
+            </View>
+          );
+        }}
+        options={{
+          title: 'Start Here',
+          headerRight: () => <Button title={'Show'} />,
+        }}
+      />
+      <Stack.Screen
+        name="LastScreen"
+        component={() => {
+          return (
+            <View style={styles.container}>
+              <Text>Bottom Tab 1</Text>
+            </View>
+          );
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const Drawer = createDrawerNavigator();
+
+const MyDrawer = () => {
+  return (
+    <Drawer.Navigator
+      openByDefault={true}
+      drawerType="slide"
+      drawerStyle={styles.drawer}
+      drawerContentOptions={{
+        activeTintColor: 'red',
+      }}>
+      <Drawer.Screen
+        name={'Stack'}
+        component={MyStackNavigator}
+        options={{
+          drawerLabel: 'Welcome Home',
+          drawerIcon: () => <Ionicons name={'home'} size={20} />,
+        }}
+      />
+      <Drawer.Screen
+        name={'Contacts'}
+        component={BottomTabs}
+        options={{
+          drawerIcon: () => <Ionicons name={'people'} size={20} />,
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+};
 
 export default App;
