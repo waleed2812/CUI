f235cc4f9786521f6638f79fab8699d89db28b1b
waleedbutt98 <waleed3072@gmail.com>
2020-12-26 14:22:47 +0500

    World Statistics Screen

--- App.js
+++ App.js
@@ -4,24 +4,42 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {Button} from 'react-native';
+import {Button, View, Text} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 // import {Ionicons} from '@expo/vector-icons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 // My Components
 import Stats from './screen-components/Stats';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {styles} from './constants/style';
+
+const Sample = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Sample</Text>
+    </View>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+const Countries = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name={'All'} component={Sample} />
+      <Tab.Screen name={'Favs'} component={Sample} />
+    </Tab.Navigator>
+  );
+};
 
 const Drawer = createDrawerNavigator();
 
 const MyDrawer = () => {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen
-        name={'World Stats'}
-        component={Stats}
-        initialParams={{stats: 'world'}}
-      />
+      <Drawer.Screen name={'World'} component={Stats} />
+      <Drawer.Screen name={'Countries'} component={Countries} />
     </Drawer.Navigator>
   );
 };
--- screen-components/Stats.js
+++ screen-components/Stats.js
@@ -1,94 +1,169 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
-import {styles} from '../constants/style';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {styles} from '../constants/style';
 
 const Stack = createStackNavigator();
 
-const StatsView = ({route}) => {
-  return (
-    <View style={styles.container}>
-      <Text>Confirmed: {route.params?.list.confirmed}</Text>
-      <Text>Recovered: {route.params?.list.recovered}</Text>
-      <Text>Critical Cases: {route.params?.list.critical}</Text>
-      <Text>Deaths: {route.params?.list.deaths}</Text>
-      <Text>Last Updated: {route.params?.list.lastUpdate}</Text>
-    </View>
-  );
-};
+const Stats = ({navigation, route}) => {
+  // Hooks
+  const no_data = [
+    {
+      confirmed: 'No Data',
+      recovered: 'No Data',
+      critical: 'No Data',
+      deaths: 'No Data',
+      lastChange: 'No Data',
+      lastUpdate: 'No Data',
+    },
+  ];
+  const [list, setList] = useState(no_data); // To Update COVID Data
+  const [population, setPopulation] = useState(0);
 
-const Stats = ({naviagtion, route}) => {
-  const [list, setList] = useState([{}]);
-  const [rendering, setRendering] = useState(false);
+  // Variables for Api Link
+  const world = route.params?.world || 'worldpopulation';
+  const cntry = route.params?.cntry || '';
+  const covid = route.params?.covid || 'totals';
 
-  let options, title;
-
-  switch (route.params?.stats) {
-    case 'world':
-      options = {
-        method: 'GET',
-        url: 'https://covid-19-data.p.rapidapi.com/totals',
-        headers: {
-          'x-rapidapi-key':
-            '067da5ff5bmsh74944382777f27dp18db32jsne4841f51b22e',
-          'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
-        },
-      };
-      title = 'World Statiscs';
-      break;
-    default:
-      options = {
-        method: 'GET',
-        url: 'https://covid-19-data.p.rapidapi.com/totals',
-        headers: {
-          'x-rapidapi-key':
-            '067da5ff5bmsh74944382777f27dp18db32jsne4841f51b22e',
-          'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
-        },
-      };
-      title = 'World Statiscs';
-      break;
-  }
-
-  const getData = () => {
+  // Function to get Data from Covid API
+  const getCovid = () => {
+    // Options for API
+    const options = {
+      method: 'GET',
+      url: 'https://covid-19-data.p.rapidapi.com/' + covid,
+      params: {name: cntry},
+      headers: {
+        'x-rapidapi-key': '067da5ff5bmsh74944382777f27dp18db32jsne4841f51b22e',
+        'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
+      },
+    };
     axios
       .request(options)
       .then(function (response) {
         // Update List from response
         setList(response.data);
 
-        // To Stop re-render
-        setRendering(true);
-
         // Save on Local Storage for later use
         AsyncStorage.setItem('list', JSON.stringify(response.data));
       })
-      .catch(function (error) {
-        console.error(error);
+      .catch(async () => {
+        // Get Data From Memory if Online Fails
+        const data = await AsyncStorage.getItem('list');
+
+        // Checking If Data was fetched
+        if (JSON.parse(data).length > 0) {
+          setList(JSON.parse(data));
+        } else {
+          // Set Empty list if no data was found
+          setList(no_data);
+        }
       });
   };
 
-  React.useEffect(getData, []);
+  // Function to get Data from World Population API
+  const getWorld = () => {
+    // Options for API
+    const options = {
+      method: 'GET',
+      url: 'https://world-population.p.rapidapi.com/' + world,
+      params: {country_name: cntry},
+      headers: {
+        'x-rapidapi-key': '067da5ff5bmsh74944382777f27dp18db32jsne4841f51b22e',
+        'x-rapidapi-host': 'world-population.p.rapidapi.com',
+      },
+    };
+    axios
+      .request(options)
+      .then(function (response) {
+        // Update List from response
+        setPopulation(response.data?.body.world_population);
 
-  if (!rendering) {
+        // Save on Local Storage for later use
+        AsyncStorage.setItem(
+          'population',
+          `${response.data.body.world_population}`,
+        );
+      })
+      .catch(async () => {
+        // Get Data From Memory if Online Fails
+
+        // Checking If Data was fetched
+        try {
+          const data = await AsyncStorage.getItem('population');
+          setPopulation(data);
+        } catch (err) {
+          // Set Empty list if no data was found
+          setPopulation(0);
+        }
+      });
+  };
+
+  React.useEffect(() => {
+    getCovid();
+    getWorld();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const Stats = () => {
+    // Variables for calculation and data display
+    const confirmed = Number.parseInt(list[0].confirmed, 10);
+    const recovered = Number.parseInt(list[0].recovered, 10);
+    const critical = Number.parseInt(list[0].critical, 10);
+    const deaths = Number.parseInt(list[0].deaths, 10);
+    const lastUpdate = list[0].lastUpdate;
+    const pop = Number.parseInt(population, 10);
+
     return (
       <View style={styles.container}>
-        <Text>No Data</Text>
+        <Text>
+          Confirmed: {confirmed} {((confirmed / pop) * 100).toFixed(2)}%
+        </Text>
+        <Text>
+          Recovered: {recovered} {((recovered / confirmed) * 100).toFixed(2)}%
+        </Text>
+        <Text>
+          Critical Cases: {critical} {((critical / confirmed) * 100).toFixed(2)}
+          %
+        </Text>
+        <Text>
+          Deaths: {deaths} {((deaths / confirmed) * 100).toFixed(2)}%
+        </Text>
+        <Text>Last Updated: {lastUpdate}</Text>
+        <Text>Total Population: {pop}</Text>
       </View>
     );
-  } else {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen
-          name={title}
-          component={StatsView}
-          initialParams={{list: list[0]}}
-        />
-      </Stack.Navigator>
-    );
-  }
+  };
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerRight: () => (
+          <TouchableOpacity onPress={getCovid}>
+            <Ionicons name={'refresh'} size={30} color={'black'} />
+          </TouchableOpacity>
+        ),
+
+        headerLeft: () => (
+          <TouchableOpacity onPress={navigation.toggleDrawer}>
+            <Ionicons name={'menu'} size={30} color={'black'} />
+          </TouchableOpacity>
+        ),
+      }}>
+      <Stack.Screen
+        name={
+          cntry.length <= 0
+            ? 'World Statistics'
+            : `${cntry.charAt(0).toUpperCase()}${cntry
+                .substr(1)
+                .toLowerCase()} Statistics`
+        }
+        component={Stats}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default Stats;
