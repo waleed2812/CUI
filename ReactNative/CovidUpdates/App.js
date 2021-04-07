d84b946f659ab536bc85829fd45dd3356a7bf564
Waleed Butt <56509403+waleedbutt98@users.noreply.github.com>
2021-04-07 06:54:03 +0500

    Starting Lecture 14

--- ReactNative/CovidUpdates/App.js
+++ ReactNative/CovidUpdates/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
