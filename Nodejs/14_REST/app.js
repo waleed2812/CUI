ff8773fa07511363f98e8cbd87662ca2001a0357
Waleed Butt <56509403+waleedbutt98@users.noreply.github.com>
2021-04-18 04:17:44 +0500

    App.js 1st Done

--- Nodejs/14_REST/app.js
+++ Nodejs/14_REST/app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express'),
     path = require('path'),
     glob = require('glob'),
@@ -24,15 +25,44 @@ require('./config/mongooseConnection')((err) => {
         global.server.listen(global.config.PORT);
         global.server.on('error', expressListeners.onError);
         global.server.on('listening', expressListeners.onListening);
+
+        app.use(express.static(path.join(__dirname, 'public')));
+
+        let webRoutes = 'app/module/**/*.routes.js';
+
+        glob.sync(webRoutes).forEach((file) => {
+            require('./' + file)(app, '');
+            console.log(file + ' is loaded');
+        });
+
+        app.use( async (err, req, res, next) => {
+            winston.error(err);
+            res.status(err.status || 500);
+                
+            if (err & err.hasOwnProperty('msgCode')) {
+                let errorCode = err.msgCode;
+                return res.json({
+                    success: 0,
+                    message: global.errors(errorCode);
+                    response: 200,
+                    data: {}
+                });
+            } else {
+                return res.json({
+                    success: 0,
+                    message: 'Something went wrong on server Side',
+                    response: 200,
+                    data: {}
+                });
+            }
+        });
+
+        //catch 404 and forward to error handler
+        app.use((err, req, res, next) => {
+
+            const err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        });
     }
-
 });
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// let webRoutes = 'app/module/**/*.routes.js';
-
-// glob.sync(webRoutes).forEach((file) => {
-//     require('./' + file)(app, '');
-//     console.log(file + ' is loaded');
-// });
