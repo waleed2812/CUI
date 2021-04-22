21f157404c470e49d8d7ab255c74d7a4f40284ff
Waleed Butt <56509403+waleedbutt98@users.noreply.github.com>
2021-04-22 18:37:32 +0500

    Teacher routes added and tested

---
+++ Nodejs/A3/app/modules/head/controllers/head.controller.js
@@ -0,0 +1,61 @@
+const winston = require('../../../../config/winston'),
+    mongoose = require('mongoose'),
+    userAccountModel = mongoose.model('userAccounts'),
+    bcrypt = require('bcryptjs');
+
+// Assignment 3
+
+
+const head = async function(req, res, next) {
+    return res.json({
+        status: 0,
+        message: 'head',
+        data: {}
+    });
+}
+const getClass = async function(req, res, next) {
+    return res.json({
+        status: 0,
+        message: 'class',
+        data: {}
+    });
+}
+const resultsClass = async function(req, res, next) {
+    return res.json({
+        status: 0,
+        message: 'resultsClass',
+        data: {}
+    });
+}
+const resultsStd = async function(req, res, next) {
+    return res.json({
+        status: 0,
+        message: 'resultsStd',
+        data: {}
+    });
+}
+const materials = async function(req, res, next) {
+    return res.json({
+        status: 0,
+        message: 'materials',
+        data: {}
+    });
+}
+const graph = async function(req, res, next) {
+    return res.json({
+        status: 0,
+        message: 'graph',
+        data: {}
+    });
+}
+
+
+
+module.exports = {
+    head,
+    getClass,
+    resultsClass,
+    resultsStd,
+    materials,
+    graph
+}
---
+++ Nodejs/A3/app/modules/head/routes/head.routes.js
@@ -0,0 +1,13 @@
+const headController = require('../controllers/head.controller');
+
+module.exports = (app, version) => {
+    // Assignment 3
+
+    app.get(version + '/head', headController.head);
+    app.get(version + '/head/class', headController.getClass);
+    app.get(version + '/head/results/class/:id', headController.resultsClass);
+    app.get(version + '/head/results/student/:id', headController.resultsStd);
+    app.get(version + '/materials', headController.materials);
+    app.get(version + '/graph', headController.graph);
+
+}
