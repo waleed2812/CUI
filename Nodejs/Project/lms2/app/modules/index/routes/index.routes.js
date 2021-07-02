c3a3b99f2e9bfa3130d7ce990b3f38de6e771cfd
Waleed Butt <56509403+waleedbutt98@users.noreply.github.com>
2021-07-02 20:56:57 +0500

    Shifting

--- Nodejs/Project/lms2/app/modules/index/routes/index.routes.js
+++
@@ -1,6 +0,0 @@
-const indexController = require('../controllers/index.controller');
-
-module.exports = (app, version) => {
-    app.get(version + '/', indexController.indexMainFunction);
-    app.get(version + '/error', indexController.errorFunction);
-};
